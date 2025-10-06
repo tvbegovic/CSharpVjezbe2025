916adc15c6436d1e0364857cd0b2c1e607e960b1
Tvrtko <tvrtkobegovic@gmail.com>
2025-10-06 11:11:16 +0200

    Add project files.

---
+++ Inicijali/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Ime: ");
+string ime = Console.ReadLine();
+Console.Write("Prezime: ");
+string prezime = Console.ReadLine();
+Console.WriteLine($"Vaši inicijali su: {ime.Substring(0, 1)}. {prezime.Substring(0, 1)}");
---
+++ TocnoVrijeme/Program.cs
@@ -0,0 +1,9 @@
+﻿//Varijable ubacujemo na stari način sa {0}
+Console.WriteLine("Točno vrijeme je: {0}", DateTime.Now);
+//Varijable ubacujemo na novi način
+Console.WriteLine($"Točno vrijeme je: {DateTime.Now}");
+
+
+
+
+
