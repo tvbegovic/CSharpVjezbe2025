9c104ca89b8add22aeab9992071bebf0bc7918aa
Tvrtko <tvrtkobegovic@gmail.com>
2025-10-10 12:58:09 +0200

    prosjek ocjena ver1

--- Inicijali/Program.cs
+++ Inicijali/Program.cs
@@ -1,5 +1,17 @@
 ﻿Console.Write("Ime: ");
-string ime = Console.ReadLine();
+string? ime = Console.ReadLine();
+if (string.IsNullOrWhiteSpace(ime))
+{
+    Console.WriteLine("Ime ne može biti prazno.");
+    return;
+}
+
 Console.Write("Prezime: ");
-string prezime = Console.ReadLine();
-Console.WriteLine($"Vaši inicijali su: {ime.Substring(0, 1)}. {prezime.Substring(0, 1)}");
+string? prezime = Console.ReadLine();
+if (string.IsNullOrWhiteSpace(prezime))
+{
+    Console.WriteLine("Prezime ne može biti prazno.");
+    return;
+}
+
+Console.WriteLine($"Vaši inicijali su: {ime.Substring(0, 1)}. {prezime.Substring(0, 1)}.");
---
+++ ProsjekOcjena/Program.cs
@@ -0,0 +1,19 @@
+﻿try
+{
+	Console.Write("Ocjena C: ");
+	string unos = Console.ReadLine();
+	int ocjenaC = int.Parse(unos);
+	Console.Write("Ocjena C++: ");
+	unos = Console.ReadLine();
+	int ocjenaCPlus = int.Parse(unos);
+	Console.Write("Ocjena C#: ");
+	unos = Console.ReadLine();
+	int ocjenaCSharp = int.Parse(unos);
+	//stavljamo 3.0 da dijeljenje bude realno (rezultat realni broj)
+	double prosjek = (ocjenaC + ocjenaCPlus + ocjenaCSharp) / 3.0;
+	Console.WriteLine($"Prosjek ocjena je {prosjek:N2}");
+}
+catch (Exception)
+{
+	Console.WriteLine("Došlo je do pogreške. Probajte ponovno");
+}
