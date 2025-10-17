3b2ca4e4b4ee49d29f48bc9bd597b841e08b9b62
Tvrtko <tvrtkobegovic@gmail.com>
2025-10-17 12:55:54 +0200

    switch

---
+++ DaniMjeseca/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.Write("Mjesec (1-12): ");
+string unos = Console.ReadLine();
+bool ok = int.TryParse(unos, out int mjesec);
+if (!ok)
+{
+    Console.WriteLine("Pogrešan format broja");
+    return;
+}
+if(mjesec < 1 ||  mjesec > 12)
+{
+    Console.WriteLine("Mjesec je izvan raspona");
+    return;
+}
+int brojDanaUMjesecu;
+string dani = "dana";
+switch(mjesec)
+{
+    case 2:
+        if (DateTime.IsLeapYear(DateTime.Now.Year))
+            brojDanaUMjesecu = 29;
+        else
+            brojDanaUMjesecu = 28;
+        break;
+    case 4:
+    case 6:
+    case 9:
+    case 11:
+        brojDanaUMjesecu = 30;
+        break;
+    default:
+        brojDanaUMjesecu = 31;
+        dani = "dan";
+        break;
+}    
+Console.WriteLine($"{mjesec}. mjesec ima {brojDanaUMjesecu} {dani}.");
---
+++ PogadanjeBroja/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine("Zamislio sam broj od 1-10. Probaj ga pogoditi.");
+Console.Write("Unesi broj: ");
+string unos = Console.ReadLine();
+bool ok = int.TryParse(unos, out int broj);
+if(!ok)
+{
+    Console.WriteLine("Pogrešan format");
+    return;
+}
+if(broj < 1 ||  broj > 10)
+{
+    Console.WriteLine("Broj je izvan raspona");
+    return;
+}
+Random random = new Random();
+int zamisljeniBroj = random.Next(1, 11);
+int razlika = Math.Abs(zamisljeniBroj - broj);
+switch(razlika)
+{
+    case 0:
+        Console.WriteLine("Sjajno, pogodili ste broj. Imate čokoladu");
+        break;
+    case 1:
+    case 2:
+    case 3:
+        Console.WriteLine($"Broj je {zamisljeniBroj}. Jako dobro. Osvajate pola čokolade");
+        break;
+    case 4:
+    case 5:
+    case 6:
+        Console.WriteLine($"Broj je {zamisljeniBroj}. Solidno. Dobili ste jedno rebro čokolade");
+        break;
+    default:
+        Console.WriteLine("Više sreće drugi put");
+        break;
+}
+    
