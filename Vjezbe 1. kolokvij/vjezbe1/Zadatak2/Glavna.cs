bab43551d709931a0f21c41f82d0b797d7f15d51
tvbegovic <prof@KAT33>
2025-11-21 09:45:08 +0100

    vjezba 1 za kolokvij

---
+++ Vjezbe 1. kolokvij/vjezbe1/Zadatak2/Glavna.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Zadatak2
+{
+    public partial class Glavna : Form
+    {
+        List<Polaznik> polaznici = new List<Polaznik>();
+        public Glavna()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDodaj_Click(object sender, EventArgs e)
+        {
+            string ime = txtIme.Text;
+            string prezime = txtPrezime.Text;
+            string oib = txtOib.Text;
+            string unos = txtDatumUpisa.Text;
+            bool ok = DateTime.TryParse(unos, out DateTime datum);
+            if (!ok)
+            {
+                MessageBox.Show("Pogrešan format datuma");
+                return;
+            }
+            unos = txtDug.Text;
+            ok = double.TryParse(unos, out double dug);
+            if (!ok)
+            {
+                MessageBox.Show("Pogrešan format duga");
+                return;
+            }
+            Polaznik polaznik = new Polaznik();
+            try
+            {
+                polaznik.Ime = ime;
+                polaznik.Prezime = prezime;
+                polaznik.Oib = oib;
+                polaznik.DatumUpisa = datum;
+                polaznik.Dug = dug;
+                polaznici.Add(polaznik);
+                AzurirajGrid();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Dogodila se pogreška. Tekst: {ex.Message}");
+            }
+
+        }
+        void AzurirajGrid()
+        {
+            dgvPolaznici.DataSource = null;
+            dgvPolaznici.DataSource = polaznici;
+        }
+
+        private void btnUcitaj_Click(object sender, EventArgs e)
+        {
+            string[] redci = File.ReadAllLines("polaznici.txt");
+            foreach (var red in redci)
+            {
+                string[] stupci = red.Split(';');
+                Polaznik polaznik = new Polaznik();
+                polaznik.Ime = stupci[0];
+                polaznik.Prezime= stupci[1];
+                polaznik.Oib= stupci[2];
+                bool ok = DateTime.TryParse(stupci[3], out DateTime datum);
+                if(ok)
+                    polaznik.DatumUpisa = datum;
+                ok = double.TryParse(stupci[4], out double dug);
+                if (ok)
+                    polaznik.Dug = dug;
+                polaznici.Add(polaznik);
+            }
+            AzurirajGrid();
+        }
+    }
+}
---
+++ Vjezbe 1. kolokvij/vjezbe1/Zadatak2/Polaznik.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadatak2
+{
+    internal class Polaznik
+    {
+        private string ime;
+        private string prezime;
+        private string oib;
+        private DateTime datumUpisa;
+        private double dug;
+
+        public string Ime { get => ime; 
+            set 
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new ArgumentException("Ime ne smije biti prazno"); ;
+                ime = value; 
+            } 
+        }
+        //TODO
+        public string Prezime { get => prezime; set => prezime = value; }
+        public string Oib { get => oib; 
+            set 
+            {
+                if (value.Length != 11)
+                    throw new ArgumentException("Oib mora imati 11 znakova");
+                oib = value; 
+            } 
+        }
+        public DateTime DatumUpisa { get => datumUpisa; 
+            set 
+            {
+                if (value > DateTime.Now)
+                    throw new ArgumentException("Datum ne smije biti u budućnosti");
+                datumUpisa = value; 
+            } 
+        }
+        public double Dug { get => dug; 
+            set 
+            {
+                if (value < 0)
+                    throw new ArgumentException("Dug ne smije biti negativan");
+                dug = value; 
+            } 
+        }
+    }
+}
